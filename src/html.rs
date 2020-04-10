4d278b020a4a007f4edfce791c11862d76b20bda
Quinten Palmer <quintenpalmer@gmail.com>
2020-04-10 15:32:34 -0400

    Introduce bad request and internal server error responses
    We will leverage this in upcoming logic.

--- src/html.rs
+++ src/html.rs
@@ -1,3 +1,14 @@
 pub fn not_found() -> String {
     "<html><body><h1>not found</h1></body></html>".into()
 }
+
+pub fn internal_server_error() -> String {
+    "<html><body><h1>internal server error</h1></body></html>".into()
+}
+
+pub fn bad_request<T: Into<String>>(message: T) -> String {
+    format!(
+        "<html><body><h1>bad request</h1><p>{}</p></body></html>",
+        message.into()
+    )
+}
--- src/http.rs
+++ src/http.rs
@@ -29,3 +29,16 @@ fn not_found_response() -> Result<Response<Body>, hyper::Error> {
     *not_found.status_mut() = StatusCode::NOT_FOUND;
     Ok(not_found)
 }
+
+fn internal_server_error<T: Into<String>>(log_message: T) -> Result<Response<Body>, hyper::Error> {
+    println!("internal server error: {}", log_message.into());
+    let mut not_found = Response::new(Body::from(html::internal_server_error()));
+    *not_found.status_mut() = StatusCode::INTERNAL_SERVER_ERROR;
+    Ok(not_found)
+}
+
+fn bad_request_response<T: Into<String>>(message: T) -> Result<Response<Body>, hyper::Error> {
+    let mut not_found = Response::new(Body::from(html::bad_request(message)));
+    *not_found.status_mut() = StatusCode::BAD_REQUEST;
+    Ok(not_found)
+}
