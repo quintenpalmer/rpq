bac4eb60ebcf625abb36b1a87ad99d8a06e0fe89
Quinten Palmer <quintenpalmer@gmail.com>
2020-04-11 17:07:38 -0400

    Store and retrieve map information next to display information

--- src/db.rs
+++ src/db.rs
@@ -6,16 +6,27 @@ use serde::{Deserialize, Serialize};
 use super::models;
 
 const DISPLAY_DB_FILE_NAME: &'static str = "db/display.csv";
+const MAP_DB_FILE_NAME: &'static str = "db/map.csv";
 
 const ALL_DB_FILE_NAMES: &'static [&'static str] = &[
     DISPLAY_DB_FILE_NAME,
+    MAP_DB_FILE_NAME,
 ];
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 struct DBDisplay {
     id: u32,
+    map_id: u32,
     cursor_x: u32,
     cursor_y: u32,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+struct DBMap {
+    id: u32,
+    default_terrain: models::Terrain,
+    hint_max_x: u32,
+    hint_max_y: u32,
 }
 
 pub struct DB {
@@ -40,8 +51,11 @@ impl DB {
         Ok(self
             .read_db_displays()?
             .into_iter()
-            .map(|x| display_model_from_db(x))
-            .collect())
+            .map(|x| {
+                let map = self.get_db_map(x.map_id)?;
+                Ok(display_model_from_db(x, map))
+            })
+            .collect::<Result<Vec<models::Display>, String>>()?)
     }
 
     pub fn get_display(&self, display_id: u32) -> Result<models::Display, String> {
@@ -69,6 +83,8 @@ impl DB {
     }
 
     pub fn add_display(&self) -> Result<(), String> {
+        let map = self.add_db_map()?;
+
         let mut records = self.read_db_displays()?;
         let max_id = records
             .iter()
@@ -76,11 +92,70 @@ impl DB {
 
         records.push(DBDisplay {
             id: max_id + 1,
+            map_id: map.id,
             cursor_x: 0,
             cursor_y: 0,
         });
 
         self.write_db_displays(records)
+    }
+
+    fn add_db_map(&self) -> Result<DBMap, String> {
+        let mut records = self.read_db_maps()?;
+        let max_id = records
+            .iter()
+            .fold(0, |acc, record| std::cmp::max(acc, record.id));
+
+        let new_record = DBMap {
+            id: max_id + 1,
+            default_terrain: models::Terrain::Grass,
+            hint_max_x: 25,
+            hint_max_y: 15,
+        };
+
+        records.push(new_record.clone());
+
+        self.write_db_maps(records)?;
+
+        Ok(new_record)
+    }
+
+    fn get_db_map(&self, map_id: u32) -> Result<DBMap, String> {
+        for record in self.read_db_maps()?.into_iter() {
+            if record.id == map_id {
+                return Ok(record);
+            }
+        }
+        return Err("could not find record with supplied id".into());
+    }
+
+    fn read_db_maps(&self) -> Result<Vec<DBMap>, String> {
+        let mut rdr = csv::Reader::from_reader(
+            File::open(MAP_DB_FILE_NAME)
+                .map_err(|e| format!("could not read from file: {:?}", e))?,
+        );
+        let records = rdr
+            .deserialize()
+            .into_iter()
+            .map(|result| -> Result<DBMap, String> {
+                result.map_err(|e| format!("could not read row for display: {:?}", e))
+            })
+            .collect::<Result<Vec<DBMap>, String>>()?;
+        Ok(records)
+    }
+
+    fn write_db_maps(&self, records: Vec<DBMap>) -> Result<(), String> {
+        let mut writer = csv::Writer::from_path(MAP_DB_FILE_NAME).unwrap();
+
+        for record in records.into_iter() {
+            writer.serialize(record).unwrap();
+        }
+        match writer.flush() {
+            Ok(()) => (),
+            Err(e) => return Err(format!("error flushing: {:?}", e)),
+        };
+
+        Ok(())
     }
 
     pub fn update_display_cursor(&self, id: u32, cursor: (u32, u32)) -> Result<(), String> {
@@ -115,11 +190,11 @@ impl DB {
     }
 }
 
-fn display_model_from_db(d: DBDisplay) -> models::Display {
+fn display_model_from_db(d: DBDisplay, m: DBMap) -> models::Display {
     models::Display {
         id: d.id,
         map: models::Map {
-            default_terrain: models::Terrain::Grass,
+            default_terrain: m.default_terrain,
             specified_terrain: (0..12)
                 .into_iter()
                 .map(|i| ((9, i), models::Terrain::Dirt))
@@ -156,8 +231,8 @@ fn display_model_from_db(d: DBDisplay) -> models::Display {
             ]
             .into_iter()
             .collect::<BTreeMap<_, _>>(),
-            hint_max_x: 12,
-            hint_max_y: 15,
+            hint_max_x: m.hint_max_x,
+            hint_max_y: m.hint_max_y,
         },
         current_selection: (d.cursor_x, d.cursor_y),
     }
--- src/models.rs
+++ src/models.rs
@@ -1,5 +1,7 @@
 use std::collections::BTreeMap;
 
+use serde::{Deserialize, Serialize};
+
 #[derive(Debug)]
 pub enum Direction {
     Right,
@@ -29,7 +31,7 @@ impl Direction {
     }
 }
 
-#[derive(Clone)]
+#[derive(Serialize, Deserialize, Clone)]
 pub enum Terrain {
     Grass,
     Dirt,
